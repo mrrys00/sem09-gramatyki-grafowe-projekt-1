36e5d4b84220fdcb9a6acb5eca0e7d2de2035d6f
Szymon Ryś <rysszymon00@gmail.com>
2024-11-19 13:04:25 +0100

    revert: main

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ from productions.utils import visualize_graph, prepare_valid_test_graph_with_han
 
 if __name__ == '__main__':
 
-    # G = prepare_valid_test_graph_with_hanging_node()
-    G = nx.Graph()
-    G.add_node('Q:5.0:5.0', label='Q', R=1)
-    G.add_nodes_from([
-        ('v:0.0:0.0', {'label': 'v', 'x': 0.0, 'y': 0.0, 'h': 0}),
-        ('v:10.0:0.0', {'label': 'v', 'x': 10.0, 'y': 0.0, 'h': 0}),
-        ('v:10.0:10.0', {'label': 'v', 'x': 10.0, 'y': 10.0, 'h': 0}),
-        ('v:0.0:10.0', {'label': 'v', 'x': 0.0, 'y': 10.0, 'h': 0})
-    ])
-    G.add_edges_from([
-        ('v:0.0:0.0', 'v:10.0:0.0', {'label': 'E', 'B': 1}),
-        ('v:10.0:0.0', 'v:10.0:10.0', {'label': 'E', 'B': 1}),
-        ('v:10.0:10.0', 'v:0.0:10.0', {'label': 'E', 'B': 1}),
-        ('v:0.0:10.0', 'v:0.0:0.0', {'label': 'E', 'B': 1}),
-        ('Q:5.0:5.0', 'v:0.0:0.0'), ('Q:5.0:5.0', 'v:10.0:0.0'), ('Q:5.0:5.0', 'v:10.0:10.0'), ('Q:5.0:5.0', 'v:0.0:10.0')
-    ])
+    G = prepare_valid_test_graph_with_hanging_node()
+    # G = nx.Graph()
+    # G.add_node('Q:5.0:5.0', label='Q', R=1)
+    # G.add_nodes_from([
+    #     ('v:0.0:0.0', {'label': 'v', 'x': 0.0, 'y': 0.0, 'h': 0}),
+    #     ('v:10.0:0.0', {'label': 'v', 'x': 10.0, 'y': 0.0, 'h': 0}),
+    #     ('v:10.0:10.0', {'label': 'v', 'x': 10.0, 'y': 10.0, 'h': 0}),
+    #     ('v:0.0:10.0', {'label': 'v', 'x': 0.0, 'y': 10.0, 'h': 0})
+    # ])
+    # G.add_edges_from([
+    #     ('v:0.0:0.0', 'v:10.0:0.0', {'label': 'E', 'B': 1}),
+    #     ('v:10.0:0.0', 'v:10.0:10.0', {'label': 'E', 'B': 1}),
+    #     ('v:10.0:10.0', 'v:0.0:10.0', {'label': 'E', 'B': 1}),
+    #     ('v:0.0:10.0', 'v:0.0:0.0', {'label': 'E', 'B': 1}),
+    #     ('Q:5.0:5.0', 'v:0.0:0.0'), ('Q:5.0:5.0', 'v:10.0:0.0'), ('Q:5.0:5.0', 'v:10.0:10.0'), ('Q:5.0:5.0', 'v:0.0:10.0')
+    # ])
     # G.add_nodes_from([
     #     ('v1', {'label': 'v', 'x': 0, 'y': 0, 'h': 0}),
     #     ('v2', {'label': 'v', 'x': 1, 'y': 0, 'h': 0}),
@@ -43,19 +43,17 @@ if __name__ == '__main__':
     #     ('Q', 'v1'), ('Q', 'v2'), ('Q', 'v3'), ('Q', 'v4'), ('Q', 'v5')
     # ])
 
-    visualize_graph(G)
+    # visualize_graph(G)
 
     prod1 = ProductionP1(G)
-    prod1.apply()
-    visualize_graph(G)
-    prod1.apply()
+    # prod1.apply()
+    # visualize_graph(G)
+    # prod1.apply()
     # prod1.apply()
     # # prod1.apply()
-    visualize_graph(G)
+    # visualize_graph(G)
     prod2 = ProductionP2(G)
     # visualize_graph(G)
-    # visualize_graph(G)
-
     prod2.apply()
     # prod2.apply()
     # prod2.apply()
