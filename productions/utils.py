2f58cae403e2b068d72a17be78081c53c95e701b
Jakub Domogała <78169141+Jakub-Domogala@users.noreply.github.com>
2024-11-23 17:35:21 +0100

    make it work not only for Q multiedges, plus show label (#3)
    * make it work not only for Q multiedges, plus show label
    
    * fix: docstring visualize
    
    ---------
    
    Co-authored-by: Szymon Ryś <rysszymon00@gmail.com>

--- productions/utils.py
+++ productions/utils.py
@@ -2,32 +2,52 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from statistics import fmean
 
-def visualize_graph(graph: nx.Graph, title: str = 'Graph Visualization'):
+
+def visualize_graph(graph: nx.Graph, title: str = "Graph Visualization"):
     """Visualize the graph."""
-    colors = ['lightblue' if data.get('label') == 'Q' else 'lightgreen' for node, data in graph.nodes(True)]
+    multiedges_labels = ["Q", "P"]  # TODO might need to extend this list
+    colors = [
+        "lightblue" if data.get("label") in multiedges_labels else "lightgreen"
+        for node, data in graph.nodes(True)
+    ]
     labels = {}
     for node, data in graph.nodes(True):
-        if data.get('label') == 'Q':
-            labels[node] = f'R = {data.get('R')}'
+        if data.get("label") in multiedges_labels:
+            labels[node] = f"{data.get('label')}\nR = {data.get('R')}"
         else:
-            labels[node] = f'x = {data.get('x')}\ny = {data.get('y')}\nh = {data.get('h')}'
+            labels[node] = (
+                f"x = {data.get('x')}\ny = {data.get('y')}\nh = {data.get('h')}"
+            )
     edge_labels = {}
     for u, v, data in graph.edges(data=True):
-        if data.get('label') is not None:
-            edge_labels[(u, v)] = f'{data.get('label')}\nB = {data.get('B')}'
+        if data.get("label") is not None:
+            edge_labels[(u, v)] = f"{data.get('label')}\nB = {data.get('B')}"
 
     positions = {}
     for point, data in graph.nodes(True):
-        if data.get('label') == 'Q':
-            x = fmean([graph.nodes[neighbor].get('x') for neighbor in graph.neighbors(point)])
-            y = fmean([graph.nodes[neighbor].get('y') for neighbor in graph.neighbors(point)])
+        if data.get("label") in multiedges_labels:
+            x = fmean(
+                [graph.nodes[neighbor].get("x") for neighbor in graph.neighbors(point)]
+            )
+            y = fmean(
+                [graph.nodes[neighbor].get("y") for neighbor in graph.neighbors(point)]
+            )
             positions[point] = (x, y)
         else:
-            positions[point] = (data.get('x'), data.get('y'))
+            positions[point] = (data.get("x"), data.get("y"))
 
     plt.figure(figsize=(20, 20))
-    nx.draw(graph, pos=positions, with_labels=True, labels=labels, node_color=colors, node_size=3000)
-    nx.draw_networkx_edge_labels(graph, pos=positions, edge_labels=edge_labels, font_size=12)
+    nx.draw(
+        graph,
+        pos=positions,
+        with_labels=True,
+        labels=labels,
+        node_color=colors,
+        node_size=3000,
+    )
+    nx.draw_networkx_edge_labels(
+        graph, pos=positions, edge_labels=edge_labels, font_size=12
+    )
     plt.title(title)
     plt.show()
 
@@ -52,6 +72,7 @@ def prepare_valid_test_graph() -> nx.Graph:
 
     return G
 
+
 def prepare_valid_test_graph_with_hanging_node() -> nx.Graph:
     """Prepares the basic 4-nodes and one Q-node graph"""
     G = nx.Graph()
@@ -73,6 +94,7 @@ def prepare_valid_test_graph_with_hanging_node() -> nx.Graph:
     ])
 
     return G
+
 
 def prepare_corrupted_test_graph() -> nx.Graph:
     """Prepares the basic 4-nodes graph"""
