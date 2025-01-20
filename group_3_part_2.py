a3adf958592ad53bb5943fdc67f01e4e18f6cd15
Magdalena Cudak <magdalena.cudak@gmail.com>
2025-01-20 19:35:31 +0100

    prod_queue added and prod 8, 17 fixes

---
+++ group_3_part_2.py
@@ -0,0 +1,173 @@
+"""
+Group 3
+Project 1 part 2
+"""
+
+import networkx as nx
+import os
+
+from productions.p01.production01 import ProductionP1
+from productions.p02.production02 import ProductionP2
+from productions.p03.production03 import ProductionP3
+from productions.p04.production04 import ProductionP4
+from productions.p07.production07 import ProductionP7
+from productions.p08.production08 import ProductionP8
+from productions.p09.production09 import ProductionP9
+from productions.p10.production10 import ProductionP10
+from productions.p11.production11 import ProductionP11
+from productions.p16.production16 import ProductionP16
+from productions.p17.production17 import ProductionP17
+
+from productions.utils import visualize_graph
+
+OUTPUT_DIR = "./results/"
+
+
+def prepare_output_directory(p: str = OUTPUT_DIR):
+    if not os.path.exists(p):
+        os.makedirs(p)
+
+
+def init_2_graph() -> nx.Graph:
+    G = nx.Graph()
+    G.add_nodes_from([
+        ('v:0.0:0.0', {'label': 'v', 'x': 0.0, 'y': 0.0, 'h': 0}),
+        ('v:10.0:0.0', {'label': 'v', 'x': 10.0, 'y': 0.0, 'h': 0}),
+        ('v:10.0:5.0', {'label': 'v', 'x': 10.0, 'y': 5.0, 'h': 0}),
+        ('v:10.0:10.0', {'label': 'v', 'x': 10.0, 'y': 10.0, 'h': 0}),
+        ('v:0.0:10.0', {'label': 'v', 'x': 0.0, 'y': 10.0, 'h': 0}),
+
+        ('v:3.0:2.0', {'label': 'v', 'x': 3.0, 'y': 2.0, 'h': 0}),
+        ('v:7.0:2.0', {'label': 'v', 'x': 7.0, 'y': 2.0, 'h': 0}),
+        ('v:8.0:5.0', {'label': 'v', 'x': 8.0, 'y': 5.0, 'h': 0}),
+        ('v:3.0:8.0', {'label': 'v', 'x': 3.0, 'y': 8.0, 'h': 0}),
+        ('v:7.0:8.0', {'label': 'v', 'x': 7.0, 'y': 8.0, 'h': 0}),
+
+        ('P:5.0:5.0', {'label': 'P', 'R': 0}),
+
+        ('Q:5.0:1.0', {'label': 'Q', 'R': 0}),
+        ('Q:9.0:3.0', {'label': 'Q', 'R': 0}),
+        ('Q:9.0:8.0', {'label': 'Q', 'R': 0}),
+        ('Q:5.0:9.0', {'label': 'Q', 'R': 0}),
+        ('Q:2.0:5.0', {'label': 'Q', 'R': 0}),
+    ])
+    G.add_edges_from([
+        ('v:0.0:0.0', 'v:10.0:0.0', {'label': 'E', 'B': 1}),
+        ('v:10.0:0.0', 'v:10.0:5.0', {'label': 'E', 'B': 1}),
+        ('v:10.0:5.0', 'v:10.0:10.0', {'label': 'E', 'B': 1}),
+        ('v:10.0:10.0', 'v:0.0:10.0', {'label': 'E', 'B': 1}),
+        ('v:0.0:10.0', 'v:0.0:0.0', {'label': 'E', 'B': 1}),
+
+        ('v:0.0:0.0', 'v:3.0:2.0', {'label': 'E', 'B': 0}),
+        ('v:10.0:0.0', 'v:7.0:2.0', {'label': 'E', 'B': 0}),
+        ('v:10.0:5.0', 'v:8.0:5.0', {'label': 'E', 'B': 0}),
+        ('v:10.0:10.0', 'v:7.0:8.0', {'label': 'E', 'B': 0}),
+        ('v:0.0:10.0', 'v:3.0:8.0', {'label': 'E', 'B': 0}),
+
+        ('v:3.0:2.0', 'v:7.0:2.0', {'label': 'E', 'B': 0}),
+        ('v:7.0:2.0', 'v:8.0:5.0', {'label': 'E', 'B': 0}),
+        ('v:8.0:5.0', 'v:7.0:8.0', {'label': 'E', 'B': 0}),
+        ('v:7.0:8.0', 'v:3.0:8.0', {'label': 'E', 'B': 0}),
+        ('v:3.0:8.0', 'v:3.0:2.0', {'label': 'E', 'B': 0}),
+
+        # for ('P:5.0:5.0', {'label': 'P', 'R': 0}),
+        ('v:3.0:2.0', 'P:5.0:5.0'),
+        ('v:7.0:2.0', 'P:5.0:5.0'),
+        ('v:8.0:5.0', 'P:5.0:5.0'),
+        ('v:7.0:8.0', 'P:5.0:5.0'),
+        ('v:3.0:8.0', 'P:5.0:5.0'),
+
+        # for ('Q:5.0:1.0', {'label': 'Q', 'R': 0}),
+        ('v:0.0:0.0', 'Q:5.0:1.0'),
+        ('v:10.0:0.0', 'Q:5.0:1.0'),
+        ('v:7.0:2.0', 'Q:5.0:1.0'),
+        ('v:3.0:2.0', 'Q:5.0:1.0'),
+
+        # for ('Q:9.0:3.0', {'label': 'Q', 'R': 0}),
+        ('v:7.0:2.0', 'Q:9.0:3.0'),
+        ('v:10.0:0.0', 'Q:9.0:3.0'),
+        ('v:10.0:5.0', 'Q:9.0:3.0'),
+        ('v:8.0:5.0', 'Q:9.0:3.0'),
+
+        # for ('Q:9.0:8.0', {'label': 'Q', 'R': 0}),
+        ('v:8.0:5.0', 'Q:9.0:8.0'),
+        ('v:10.0:5.0', 'Q:9.0:8.0'),
+        ('v:10.0:10.0', 'Q:9.0:8.0'),
+        ('v:7.0:8.0', 'Q:9.0:8.0'),
+
+        # for ('Q:5.0:9.0', {'label': 'Q', 'R': 0}),
+        ('v:3.0:8.0', 'Q:5.0:9.0'),
+        ('v:7.0:8.0', 'Q:5.0:9.0'),
+        ('v:10.0:10.0', 'Q:5.0:9.0'),
+        ('v:0.0:10.0', 'Q:5.0:9.0'),
+
+        # for ('Q:2.0:5.0', {'label': 'Q', 'R': 0}),
+        ('v:0.0:0.0', 'Q:2.0:5.0'),
+        ('v:3.0:2.0', 'Q:2.0:5.0'),
+        ('v:3.0:8.0', 'Q:2.0:5.0'),
+        ('v:0.0:10.0', 'Q:2.0:5.0'),
+    ])
+
+    return G
+
+
+def run_prod_queue(G: nx.Graph, init: list, triple_mark: list, triple_break: list, reference_node: dict,
+                   iterations: int) -> nx.Graph:
+    iter = 0
+    m = lambda x: '0' * (3 - len(str(x))) + str(x)
+    visualize_graph(G, title=f"{m(iter)} Initial graph", img_path=OUTPUT_DIR)
+    iter += 1
+
+    for p in init:
+        applied = p(G).apply_with_reference_node(reference_node)
+        if applied:
+            print(f"{p.__name__} applied")
+            visualize_graph(G, title=f"{m(iter)} {p.__name__} applied", img_path=OUTPUT_DIR)
+            iter += 1
+
+    for _ in range(iterations):
+
+        for p in triple_mark:
+            applied = p(G).apply_with_reference_node(reference_node)
+            if applied:
+                print(f"{p.__name__} applied")
+                visualize_graph(G, title=f"{m(iter)} {p.__name__} applied", img_path=OUTPUT_DIR)
+                iter += 1
+
+        for p in triple_break:
+            applied = p(G).apply_with_reference_node(reference_node)
+            if applied:
+                print(f"{p.__name__} applied")
+                visualize_graph(G, title=f"{m(iter)} {p.__name__} applied", img_path=OUTPUT_DIR)
+                iter += 1
+
+
+    return G
+
+if __name__ == '__main__':
+    prepare_output_directory()
+
+    G = init_2_graph()
+
+    init = [
+        ProductionP7,
+        ProductionP1,
+    ]
+
+    triple_mark = [
+        ProductionP7,
+        ProductionP8,
+        ProductionP17,
+        ProductionP8
+    ]
+
+    triple_break = [
+        ProductionP10,
+        ProductionP2,
+        ProductionP3,
+        ProductionP1,
+    ]
+    reference_node = {"x": 7.0, "y": 8.0}
+    iterations = 2
+
+    run_prod_queue(G, init, triple_mark, triple_break, reference_node, iterations)
--- productions/p08/production08.py
+++ productions/p08/production08.py
@@ -63,7 +63,14 @@ class ProductionP8(Production):
 
     def apply_with_reference_node(self, reference_node):
         """Apply P8 to the quadrilateral if possible."""
-        candidate_nodes = list(self.graph.nodes(data=True))
+        n = None
+        for node, data in self.graph.nodes(data=True):
+            if data.get('x') == reference_node.get('x') and data.get('y') == reference_node.get('y'):
+                n = node
+
+        reference_neighbors = list(self.graph.neighbors(n))
+        all_nodes = list(self.graph.nodes(data=True))
+        candidate_nodes = [(node_id,node_data) for node_id, node_data in all_nodes if node_id in reference_neighbors]
         candidate_nodes.sort(key=lambda x: sqrt((float(x[0].split(':')[1]) - reference_node['x'])**2 + (float(x[0].split(':')[2]) - reference_node['y'])**2))
 
         central_node = None
@@ -86,7 +93,7 @@ class ProductionP8(Production):
                     subgraph_b = self._check_neighbors(neighbors_n2, neighbors_n1, n2, neighbors, central_node)
 
                     result = next((sg for sg in (subgraph_a, subgraph_b) if sg is not None), None)
-                    if result:
+                    if result and result[0] in reference_neighbors:
                         self.graph.nodes[result[0]]['R'] = 1
                         return True
 
